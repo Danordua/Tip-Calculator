482e1c6670bc32a2d5d198048ec3b3cc71039ebd
Daniel Orduña <danordua@gmail.com>
2021-09-13 23:32:30 -0500

    JS v.1

--- app.js
+++ app.js
@@ -7,12 +7,52 @@
         //--------------------------------------------------------------------------------------------------------------
         /*CONSTANTS*/
 
-        const btnTip5 = document.getElementById(btn5);
+        const numBillInput = document.querySelector("#bill-number");
+        const strBillDivided = document.querySelector(".strBillText");
+        const strTipTotal = document.querySelector(".strTipText");
+        const numPeopleInput = document.querySelector("#num-people-input");
+        const btnTipOptions = document.querySelectorAll(".tip-btn");
+        const btnReset = document.querySelector(".reset");
 
         //--------------------------------------------------------------------------------------------------------------
         /*FUNCTIONS*/
 
-        console.log(btnTip5);
+        numBillInput.addEventListener('input', () => {
+            if (numPeopleInput.value == ""){
+                strBillDivided.innerText = "$" + (numBillInput.value);
+            } else {
+                strBillDivided.innerText = "$" + (numBillInput.value / numPeopleInput.value).toFixed(2);
+            }
+        });
+
+        numPeopleInput.addEventListener('input', () =>{
+            if (numPeopleInput == ""){
+                strBillDivided.innerText = "$" + (numBillInput.value);
+            } else {
+                strBillDivided.innerText = "$" + (numBillInput.value / numPeopleInput.value).toFixed(2);
+            }
+        });
+
+        (function selectTip(){
+            btnTipOptions.forEach(btn => {
+                btn.addEventListener('click', () => {
+                    
+                    let numBtnValue = btn.value.replace('%', '');
+                    if(numBillInput.value == ""){
+                        strTipTotal.innerText = "$0.00";
+                    } else {
+                        strTipTotal.innerText = "$" + (numBillInput.value * (numBtnValue / 100)).toFixed(2);
+                    }
+
+                })
+            })
+        })();
+
+        (function voidClearScreen(){
+            if (numBillInput.value != "" || numPeopleInput.value !== ""){
+                btnReset.disabled = false;
+            }
+        })
 
         /*TASK */
         //--------------------------------------------------------------------------------------------------------------
